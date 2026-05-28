b63457d98aad02d423310b80fd6a428a7d7e7121
sirkays <attehkayode2@gmail.com>
2026-05-28 15:22:13 +0100

    Implement route pay update settings

--- lekki_project/settings.py
+++ lekki_project/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -45,6 +47,39 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://lera.odoo.com",
+    "https://www.lera.odoo.com",
+    "https://leravirtualization.newwavesecosystem.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://lera.odoo.com",
+    "https://www.lera.odoo.com",
+    "https://leravirtualization.newwavesecosystem.com",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
 ]
 
 AUTH_USER_MODEL = 'authapp.CustomUser'
